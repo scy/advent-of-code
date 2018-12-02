42f923f1819dda318ac81bfb9983f388b1fbc2a9
Tim Weber <scy@scy.name>
2018-12-02 22:00:56 +0100

    Only compare with following lines

--- 2018/02/2018-02-b.js
+++ 2018/02/2018-02-b.js
@@ -9,8 +9,11 @@ let readline = require("readline").createInterface({
 let lines = [];
 readline.on("line", line => lines.push(line));
 readline.on("close", () => {
-    outer_lines: for (let line of lines) {
-        for (let other_line of lines) {
+    let number = lines.length - 1;
+    for (let i = 0; i < number; i++) {
+        let line = lines[i];
+        for (let j = i + 1; j < lines.length; j++) {
+            let other_line = lines[j];
             let samechars = "";
             for (let pos = 0; pos < line.length; pos++) {
                 if (line[pos] === other_line[pos]) {
@@ -19,7 +22,6 @@ readline.on("close", () => {
             }
             if (samechars.length === line.length - 1) {
                 console.log(samechars);
-                break outer_lines;
             }
         }
     }
