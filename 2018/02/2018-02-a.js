403a49ee27705a23c41382273ad5c03e884f1d51
Tim Weber <scy@scy.name>
2018-12-02 22:38:27 +0100

    Replace let with const where possible

--- 2018/02/2018-02-a.js
+++ 2018/02/2018-02-a.js
@@ -1,20 +1,20 @@
 "use strict";
 
-let readline = require("readline").createInterface({
+const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false, // else the input will be echoed
 });
 
-let lines = [];
+const lines = [];
 readline.on("line", line => lines.push(line));
 readline.on("close", () => {
-    let lettercounts = new Map();
+    const lettercounts = new Map();
     let doubles = 0;
     let triples = 0;
-    for (let line of lines) {
+    for (const line of lines) {
         lettercounts.clear();
-        for (let char of line) {
+        for (const char of line) {
             if (lettercounts.has(char)) {
                 lettercounts.set(char, lettercounts.get(char) + 1);
             } else {
@@ -23,7 +23,7 @@ readline.on("close", () => {
         }
         let doubles_found = false;
         let triples_found = false;
-        for (let count of lettercounts.values()) {
+        for (const count of lettercounts.values()) {
             if (count === 2) {
                 doubles_found = true;
             }
--- 2018/02/2018-02-b.js
+++ 2018/02/2018-02-b.js
@@ -1,19 +1,19 @@
 "use strict";
 
-let readline = require("readline").createInterface({
+const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false, // else the input will be echoed
 });
 
-let lines = [];
+const lines = [];
 readline.on("line", line => lines.push(line));
 readline.on("close", () => {
-    let number = lines.length - 1;
+    const number = lines.length - 1;
     for (let i = 0; i < number; i++) {
-        let line = lines[i];
+        const line = lines[i];
         for (let j = i + 1; j < lines.length; j++) {
-            let other_line = lines[j];
+            const other_line = lines[j];
             let samechars = "";
             for (let pos = 0; pos < line.length; pos++) {
                 if (line[pos] === other_line[pos]) {
