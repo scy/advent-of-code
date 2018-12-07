00e054d54484f85380efdf8947125c02bbc0f269
Tim Weber <scy@scy.name>
2018-12-07 19:51:57 +0100

    Add 2018-07-a

---
+++ 2018/07/2018-07-a.js
@@ -0,0 +1,47 @@
+const input = require("./input").input;
+
+const next = {};
+const prev = {};
+const letters = new Set();
+const built = [];
+
+function prepareData() {
+    for ({first, then} of input) {
+        letters.add(first);
+        letters.add(then);
+        if (!(first in next)) {
+            next[first] = [];
+        }
+        next[first].push(then);
+        if (!(then in prev)) {
+            prev[then] = [];
+        }
+        prev[then].push(first);
+    }
+}
+
+function ready(letter) {
+    if (!(letter in prev)) {
+        return true;
+    }
+    for (prereq of prev[letter]) {
+        if (!built.includes(prereq)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findNext() {
+    const candidates = Array.from(letters.values()).filter(letter => ready(letter) && !built.includes(letter)).sort();
+    return candidates ? candidates[0] : false;
+}
+
+prepareData();
+
+let nextLetter;
+while (nextLetter = findNext()) {
+    built.push(nextLetter);
+}
+
+console.log(built.join(""));
