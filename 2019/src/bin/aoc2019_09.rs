b3fd70464f569af18af6d571ef606e572b7993dd
Tim Weber <scy@scy.name>
2019-12-09 22:00:54 +0100

    Add solution for day 9 part 2

--- 2019/src/bin/aoc2019_09.rs
+++ 2019/src/bin/aoc2019_09.rs
@@ -2,16 +2,21 @@ use std::error::Error;
 use aoc2019::IntcodeMachine;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut machine = IntcodeMachine::from_stdin();
-    machine.push_input(1);
+    let mut test_machine = IntcodeMachine::from_stdin();
+    let mut sensor_machine = test_machine.clone();
+    test_machine.push_input(1);
+    sensor_machine.push_input(2);
 
-    machine.compute();
-    let outputs = machine.get_outputs();
+    test_machine.compute();
+    let outputs = test_machine.get_outputs();
 
     if outputs.len() != 1 {
         println!("BOOST found errors: {:?}", outputs);
     } else {
         println!("BOOST keycode: {}", outputs[0]);
+        println!("Boosting sensors...");
+        sensor_machine.compute();
+        println!("Distress signal coordinates: {}", sensor_machine.get_output());
     }
 
     Ok(())
